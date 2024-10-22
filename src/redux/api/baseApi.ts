a83831c09f5b9e8301bc52510883f51a2f054a1d
Ibrahim Sikder <ibrahimsikder5033@gmail.com>
2024-10-22 22:41:13 +0600

    baseapi-correct

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -5,7 +5,7 @@ import { tagTypesList } from "./tag-types";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: axiosBaseQuery({
-    baseUrl: `${process.env.NEXT_PUBLIC_BASE_API_URL}`,
+    baseUrl: process.env.NEXT_PUBLIC_BASE_API_URL as string,
   }),
   endpoints: () => ({}),
   tagTypes: tagTypesList,
